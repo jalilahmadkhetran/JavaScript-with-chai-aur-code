7a47f6db1421e4e057a2e25f7291beea69c886e7
jalilahmadkhetran <123321692+jalilahmadkhetran@users.noreply.github.com>
2024-09-22 20:46:46 +0000

    datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) we are using node js not browser
+
+console.log(3+3)
+
+console.log("NovaSols");
+
+
+let name="NovaSols"
+let age =20
+let isLoggedIn=false
+
+//Primative DataTypes
+//number=> 2 to power 53
+//bigInt
+//String=>""
+//Boolean=>true / false
+//nulll=>standalone Value
+//Undefined=>
+//Symbol=> Unique
+
+
+//Object
+
+console.log(typeof null);  //object
+console.log(typeof undefined);//undefined
