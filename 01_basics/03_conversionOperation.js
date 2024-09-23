f62a6d47defc538bb55d0265f7c47a103a6c19fe
jalilahmadkhetran <123321692+jalilahmadkhetran@users.noreply.github.com>
2024-09-23 13:17:57 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,60 @@
+let score ="NovaSols"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33"=> 33
+//"33abc"=> NaN Not a number
+//true=>1 : false=>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1=>true ;0=>false
+// ""=>false
+// "NovaSols"=>true
+
+let someNumber=36
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//******************Operations*****************
+
+let value=3
+
+let negValue=-value
+console.log(negValue);
+
+let str1="Nova"
+let str2=" Sols"
+let str3=str1+str2
+console.log(str3);
+
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("2"+2+2);//if string is first all will treat as string
+console.log(2+2+"2");//if string is last then thrn first conversions will be done and last will be same
+console.log((4+2)*3);
+console.log(1+2-3*4);
+
+console.log(+true);
+console.log(+"");
+
+let num1 , mum2 , num3
+num1 = num2 =num3 =2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
